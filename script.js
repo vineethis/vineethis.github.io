c037b5535135a8ba2fc14066ad0bfa2ebfed2710
Vineeth I S <53191883+vineethis@users.noreply.github.com>
2023-09-19 11:41:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,66 +1,36 @@
-let purchaseCount = 1; // Keep track of the number of purchases
-
-function updateAmountInvested() {
-    const purchaseRows = document.querySelectorAll(".purchase-row");
-    purchaseRows.forEach((row, index) => {
-        const price = parseFloat(row.querySelector(".price").value);
-        const quantity = parseInt(row.querySelector(".quantity").value);
-
-        if (!isNaN(price) && !isNaN(quantity)) {
-            const amountInvested = (price * quantity).toFixed(2);
-            row.querySelector(".amount-invested").textContent = `Amount Invested: $${amountInvested}`;
-        }
-    });
-}
+let purchases = [];
 
 function addPurchase() {
-    const purchaseRow = document.createElement("div");
-    purchaseRow.classList.add("purchase-row");
-
-    const priceLabel = document.createElement("label");
-    priceLabel.textContent = "Price:";
-
     const priceInput = document.createElement("input");
     priceInput.type = "number";
-    priceInput.classList.add("price");
+    priceInput.classList.add("input-field");
     priceInput.step = "0.01";
-
-    const quantityLabel = document.createElement("label");
-    quantityLabel.textContent = "Quantity:";
 
     const quantityInput = document.createElement("input");
     quantityInput.type = "number";
-    quantityInput.classList.add("quantity");
+    quantityInput.classList.add("input-field");
 
-    const amountInvestedSpan = document.createElement("span");
-    amountInvestedSpan.classList.add("amount-invested");
-    amountInvestedSpan.textContent = "Amount Invested: $0.00";
-
-    purchaseRow.appendChild(priceLabel);
+    const purchaseRow = document.createElement("div");
+    purchaseRow.classList.add("purchase-row");
     purchaseRow.appendChild(priceInput);
-    purchaseRow.appendChild(quantityLabel);
     purchaseRow.appendChild(quantityInput);
-    purchaseRow.appendChild(amountInvestedSpan);
 
     document.getElementById("purchase-inputs").appendChild(purchaseRow);
-
-    purchaseCount++;
 }
 
 function calculateAverage() {
-    const priceInputs = document.querySelectorAll(".price");
-    const quantityInputs = document.querySelectorAll(".quantity");
-
-    if (priceInputs.length !== quantityInputs.length || priceInputs.length === 0) {
-        alert("Please fill in all price and quantity fields.");
+    const priceInputs = document.querySelectorAll(".input-field");
+    
+    if (priceInputs.length === 0) {
+        alert("Please add at least one purchase.");
         return;
     }
 
     purchases = [];
 
-    for (let i = 0; i < priceInputs.length; i++) {
+    for (let i = 0; i < priceInputs.length; i += 2) {
         const price = parseFloat(priceInputs[i].value);
-        const quantity = parseInt(quantityInputs[i].value);
+        const quantity = parseInt(priceInputs[i + 1].value);
 
         if (isNaN(price) || isNaN(quantity)) {
             alert("Please enter valid values for price and quantity.");
@@ -74,11 +44,10 @@ function calculateAverage() {
     const totalValueSum = purchases.reduce((total, purchase) => total + (purchase.price * purchase.quantity), 0);
 
     const averagePrice = totalValueSum / totalShares;
-    document.getElementById("average-price").textContent = `$${averagePrice.toFixed(2)}`;
-}
+    const purchaseValues = purchases.map(purchase => purchase.price * purchase.quantity);
+    const totalInvestment = purchaseValues.reduce((total, value) => total + value, 0);
 
-// Listen for input changes and update the amount invested
-const inputs = document.querySelectorAll(".price, .quantity");
-inputs.forEach((input) => {
-    input.addEventListener("input", updateAmountInvested);
-});
+    document.getElementById("average-price").textContent = averagePrice.toFixed(2);
+    document.getElementById("purchase-values").textContent = purchaseValues.join(", ");
+    document.getElementById("total-investment").textContent = totalInvestment.toFixed(2);
+}
