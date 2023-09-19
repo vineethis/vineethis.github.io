bff73a8aa496e1dd46fa653b4ac1569eaa5ca674
Vineeth I S <53191883+vineethis@users.noreply.github.com>
2023-09-19 11:36:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,49 @@
 let purchaseCount = 1; // Keep track of the number of purchases
 
+function updateAmountInvested() {
+    const purchaseRows = document.querySelectorAll(".purchase-row");
+    purchaseRows.forEach((row, index) => {
+        const price = parseFloat(row.querySelector(".price").value);
+        const quantity = parseInt(row.querySelector(".quantity").value);
+
+        if (!isNaN(price) && !isNaN(quantity)) {
+            const amountInvested = (price * quantity).toFixed(2);
+            row.querySelector(".amount-invested").textContent = `Amount Invested: $${amountInvested}`;
+        }
+    });
+}
+
 function addPurchase() {
     const purchaseRow = document.createElement("div");
     purchaseRow.classList.add("purchase-row");
-    
+
     const priceLabel = document.createElement("label");
     priceLabel.textContent = "Price:";
-    
+
     const priceInput = document.createElement("input");
     priceInput.type = "number";
     priceInput.classList.add("price");
     priceInput.step = "0.01";
-    
+
     const quantityLabel = document.createElement("label");
     quantityLabel.textContent = "Quantity:";
-    
+
     const quantityInput = document.createElement("input");
     quantityInput.type = "number";
     quantityInput.classList.add("quantity");
-    
+
+    const amountInvestedSpan = document.createElement("span");
+    amountInvestedSpan.classList.add("amount-invested");
+    amountInvestedSpan.textContent = "Amount Invested: $0.00";
+
     purchaseRow.appendChild(priceLabel);
     purchaseRow.appendChild(priceInput);
     purchaseRow.appendChild(quantityLabel);
     purchaseRow.appendChild(quantityInput);
-    
+    purchaseRow.appendChild(amountInvestedSpan);
+
     document.getElementById("purchase-inputs").appendChild(purchaseRow);
-    
+
     purchaseCount++;
 }
 
@@ -58,3 +76,9 @@ function calculateAverage() {
     const averagePrice = totalValueSum / totalShares;
     document.getElementById("average-price").textContent = `$${averagePrice.toFixed(2)}`;
 }
+
+// Listen for input changes and update the amount invested
+const inputs = document.querySelectorAll(".price, .quantity");
+inputs.forEach((input) => {
+    input.addEventListener("input", updateAmountInvested);
+});
