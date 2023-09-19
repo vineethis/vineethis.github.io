9c3631cbb5176a9c773718a0c127e5fb8ba4bae7
Vineeth I S <53191883+vineethis@users.noreply.github.com>
2023-09-19 11:22:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,44 @@
 let purchases = [];
 
 function addPurchase() {
-    const price = parseFloat(document.getElementById("price").value);
-    const quantity = parseInt(document.getElementById("quantity").value);
+    const priceInput = document.createElement("input");
+    priceInput.type = "number";
+    priceInput.classList.add("price");
+    priceInput.step = "0.01";
 
-    if (isNaN(price) || isNaN(quantity)) {
-        alert("Please enter valid values for price and quantity.");
-        return;
-    }
+    const quantityInput = document.createElement("input");
+    quantityInput.type = "number";
+    quantityInput.classList.add("quantity");
 
-    purchases.push({ price, quantity });
+    const purchaseRow = document.createElement("div");
+    purchaseRow.classList.add("purchase-row");
+    purchaseRow.appendChild(priceInput);
+    purchaseRow.appendChild(quantityInput);
 
-    document.getElementById("price").value = ""; // Clear input fields
-    document.getElementById("quantity").value = "";
+    document.getElementById("purchase-inputs").appendChild(purchaseRow);
 }
 
 function calculateAverage() {
-    if (purchases.length === 0) {
-        alert("Please add at least one purchase.");
+    const priceInputs = document.querySelectorAll(".price");
+    const quantityInputs = document.querySelectorAll(".quantity");
+
+    if (priceInputs.length !== quantityInputs.length) {
+        alert("Please fill in all price and quantity fields.");
         return;
+    }
+
+    purchases = [];
+
+    for (let i = 0; i < priceInputs.length; i++) {
+        const price = parseFloat(priceInputs[i].value);
+        const quantity = parseInt(quantityInputs[i].value);
+
+        if (isNaN(price) || isNaN(quantity)) {
+            alert("Please enter valid values for price and quantity.");
+            return;
+        }
+
+        purchases.push({ price, quantity });
     }
 
     const totalShares = purchases.reduce((total, purchase) => total + purchase.quantity, 0);
