66efb365fe1eb5564ab605722c751a6e94cfcb90
Vineeth I S <53191883+vineethis@users.noreply.github.com>
2023-09-19 12:17:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -12,14 +12,14 @@ function addPurchase() {
 
     const purchaseRow = document.createElement("div");
     purchaseRow.classList.add("purchase-row");
-    
+
     // Create new labels for each purchase
     const priceLabel = document.createElement("label");
     priceLabel.textContent = "Price:";
-    
+
     const quantityLabel = document.createElement("label");
     quantityLabel.textContent = "Quantity:";
-    
+
     purchaseRow.appendChild(priceLabel);
     purchaseRow.appendChild(priceInput);
     purchaseRow.appendChild(quantityLabel);
@@ -30,7 +30,7 @@ function addPurchase() {
 
 function calculateAverage() {
     const priceInputs = document.querySelectorAll(".input-field");
-    
+
     if (priceInputs.length === 0) {
         alert("Please add at least one purchase.");
         return;
@@ -54,18 +54,23 @@ function calculateAverage() {
     const totalValueSum = purchases.reduce((total, purchase) => total + (purchase.price * purchase.quantity), 0);
 
     const averagePrice = totalValueSum / totalShares;
-    
-    const purchaseValues = purchases.map((purchase, index) => `Purchase Value for ${index + 1} Buy: ${purchase.price * purchase.quantity}`);
-    const totalInvestment = purchaseValues.reduce((total, value) => total + value, "");
+    const totalInvestment = totalValueSum; // Total investment is the sum of all purchases
 
     document.getElementById("average-price").textContent = averagePrice.toFixed(2);
-    document.getElementById("purchase-values-list").innerHTML = ""; // Clear the list
+
+    // Display the total investment as a single value
+    document.getElementById("total-investment").textContent = totalInvestment.toFixed(2);
+
+    // Create a string containing the purchase values for display
+    const purchaseValues = purchases.map((purchase, index) => `Purchase Value for ${index + 1} Buy: ${(purchase.price * purchase.quantity).toFixed(2)}`);
+    
+    // Display the purchase values as a list
+    document.getElementById("purchase-values-list").innerHTML = "";
     purchaseValues.forEach(value => {
         const li = document.createElement("li");
         li.textContent = value;
         document.getElementById("purchase-values-list").appendChild(li);
     });
-    document.getElementById("total-investment").textContent = totalInvestment;
 }
 
 function resetFields() {
